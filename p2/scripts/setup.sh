22ef8b712cfef025f2c20e17e18c7a06b0b939b4
jcammas <matoulet.cammas@gmail.com>
2022-08-04 22:09:16 +0200

    lets go p2 finito

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -2,19 +2,30 @@
 
 
 echo "[K3S] : installing..."
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $1 --node-ip $1 --flannel-iface=eth1"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $1 --node-ip $1  --flannel-iface=eth1"
 curl -sfL https://get.k3s.io |  sh -
-
-
 
 echo "[TOOLS] : install ifconfig tool ..."
 sudo yum install net-tools -y
 
 echo "[APP-1] : deploying..."
 
-/usr/local/bin/kubectl create configmap app1-html --from-file /Users/julien/42/IoT/p2/jcammasS/confs/app1/index.html
-/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app1/app1.deployment.yaml
-/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app1/app1.service.yaml
+/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app1/app1.yaml
 /usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app1/app1.ingress.yaml
 
 echo "[APP-1] : ok..." 
+
+
+echo "[APP-2] : deploying..."
+
+/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app2/app2.yaml
+/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app2/app2.ingress.yaml
+
+echo "[APP-2] : ok..." 
+
+echo "[APP-3] : deploying..."
+
+/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app3/app3.yaml
+/usr/local/bin/kubectl  apply -f /Users/julien/42/IoT/p2/jcammasS/confs/app3/app3.ingress.yaml
+
+echo "[APP-3] : ok..." 
