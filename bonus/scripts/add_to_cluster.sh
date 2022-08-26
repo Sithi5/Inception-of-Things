a6d2d5eafedacff210f345cf83831db2a1c1a92a
mathieu-az <mathieu@azmed.co>
2022-08-26 19:03:40 +0200

    add bonus

--- bonus/scripts/add_to_cluster.sh
+++ bonus/scripts/add_to_cluster.sh
@@ -1,1 +1,14 @@
+
+# colors
+DARK_GREEN='\e[0;32m'
+END='\e[0;m'
+
+echo "${DARK_GREEN}[UPDATE CLUSTER] : ADD NAMESPACE...${END}"
+
+
 sudo cluster create namespace gitlab
+
+echo "${DARK_GREEN}[UPDATE CLUSTER] : Apply modifications to argocd...${END}"
+
+kubectl apply -n argocd -f /confs/application.yaml
+kubectl apply -n argocd -f /confs/project.yaml
--- bonus/scripts/install_gitlab.sh
+++ bonus/scripts/install_gitlab.sh
@@ -8,13 +8,14 @@ END='\e[0;m'
 echo "${DARK_GREEN}[UPADTE AND INSTALL] :  DEPENDENCIES..${END}"
 
 sudo apt update
+sudo service ufw stop
 sudo apt install -y curl openssh-server ca-certificates tzdata perl
-sudo apt install -y postfix
 
 echo "${DARK_GREEN}[INSTALL] :  INSTALL GITLAB...${END}"
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-sudo EXTERNAL_URL="https://gitlab.example.com" apt install gitlab-ce
+
+sudo EXTERNAL_URL="http://192.168.42.110:8181" apt install gitlab-ce
 
 echo "${DARK_GREEN}[PASSWORD] : ${END}"
 
